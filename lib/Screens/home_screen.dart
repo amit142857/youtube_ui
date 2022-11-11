d333278b5afa8752d09ce2848d03b4b2f88dcd0b
Amit <ydvamit82@gmail.com>
2022-11-11 20:16:39 +0545

    added widget folder and modified the code

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_ui/data.dart';
+import 'package:youtube_ui/widgets/CustomSLiverAppBar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,33 +10,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          SliverAppBar(
-            leadingWidth: 100,
-            leading: Padding(
-              padding: const EdgeInsets.only(left: 12),
-              child: Image.asset("assets/yt_logo.png"),
-            ),
-            actions: [
-              IconButton(
-                icon: const Icon(Icons.cast),
-                onPressed: (){},
-              ),
-              IconButton(
-                icon: const Icon(Icons.notifications_outlined),
-                onPressed: (){},
-              ),
-              IconButton(
-                icon: const Icon(Icons.search),
-                onPressed: (){},
-              ),
-              IconButton(
-                icon: CircleAvatar(
-                  foregroundImage: NetworkImage(currentUser.profileImageUrl),
-                ),
-                onPressed: (){},
-              ),
-            ],
-          )
+            CustomSliverAppBar(),
         ],
       ),
     );
---
+++ lib/widgets/CustomSLiverAppBar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../data.dart';
+
+class CustomSliverAppBar extends StatelessWidget {
+  const CustomSliverAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      leadingWidth: 100,
+      leading: Padding(
+        padding: const EdgeInsets.only(left: 12),
+        child: Image.asset("assets/yt_logo.png"),
+      ),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.cast),
+          onPressed: (){},
+        ),
+        IconButton(
+          icon: const Icon(Icons.notifications_outlined),
+          onPressed: (){},
+        ),
+        IconButton(
+          icon: const Icon(Icons.search),
+          onPressed: (){},
+        ),
+        IconButton(
+          iconSize: 40,
+          icon: CircleAvatar(
+            foregroundImage: NetworkImage(currentUser.profileImageUrl),
+          ),
+          onPressed: (){},
+        ),
+      ],
+    );
+  }
+}
